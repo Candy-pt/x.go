78702731d54e5f8f43dbec8ddc07235cc7629f6b
ha <hitomi2k4@gmail.com>
2026-03-14 01:01:48 +0700

    add icon

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
-  plugins: [react()],
+   plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Quản lý Xưởng Gỗ X-Go',
+        short_name: 'X-Go',
+        description: 'Hệ thống quản lý sản xuất xưởng gỗ',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 5173,              // port chạy dev server
     https: false,            // để chạy HTTP bình thường, đổi thành true nếu muốn HTTPS
